04146a54d0025b669e919acecc360f854f4a6b28
Vivek-Kamboj <vivekkamboj4555@gmail.com>
2020-12-14 15:58:42 +0530

    Progess Component added

--- Frontend/crowd-funding-frontend/src/Components/campaign.jsx
+++ Frontend/crowd-funding-frontend/src/Components/campaign.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Campaign = (p) => {
-  console.log(p.match.params.id);
-  return <div>Each Campaign...</div>;
+  const [state, setState] = useState(0);
+
+  useEffect(() => setState(50), []);
+  return (
+    <React.Fragment>
+      <div>Campaign id: {p.match.params.id}</div>
+
+      <div className="col-8 p-2 m-5 border bg-warning">
+        <div className="progress ">
+          <div
+            className="progress-bar bg-danger progress-bar-animated"
+            role="progressbar"
+            aria-valuenow="75"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            style={{ width: `${state}%` }}
+          ></div>
+        </div>
+
+        <div className="row justify-content-between">
+          <div className="col-4">
+            <p>Fund Required:</p>
+            <p>#####</p>
+          </div>
+          <div className="col-4 text-right">
+            <p>Fund Raised:</p>
+            <p>#####</p>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Campaign;
