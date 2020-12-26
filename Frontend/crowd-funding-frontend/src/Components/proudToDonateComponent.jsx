42a44b1b8e4b8e052b14e1f39e86fc2756cd5f5e
Vivek-Kamboj <vivekkamboj4555@gmail.com>
2020-12-26 19:51:06 +0530

    slideshow in proud to donate added

--- Frontend/crowd-funding-frontend/src/Components/proudToDonateComponent.jsx
+++ Frontend/crowd-funding-frontend/src/Components/proudToDonateComponent.jsx
@@ -1,10 +1,46 @@
-import React from "react";
-import "./styles/proudToDonate.module.css";
+import React, { useEffect, useState } from "react";
+import styles from "./styles/proudToDonate.module.css";
 
 const ProudToDonate = () => {
+  const x = [".myactive", ".mynone"];
+  const [proprety, setProperty] = useState(x);
+  const [index, setIndex] = useState(0);
+  let n = 2;
+  const setImage = () => {
+    let i = index;
+    var current = [...proprety];
+
+    current[i] = ".mynone";
+    i = (i + 1) % n;
+
+    current[i] = ".myactive";
+    setProperty(current);
+    setIndex(i);
+  };
+
+  useEffect(() => {
+    setTimeout(setImage, 3000);
+  });
+
   return (
     <React.Fragment>
       <h1>ProudToDonate</h1>
+      <div className="col-10 m-auto">
+        <img
+          src="https://thumbs.dreamstime.com/b/poor-indian-beggar-family-street-leh-ladakh-india-june-unidentified-children-early-ages-often-brought-to-70999174.jpg"
+          alt="ProudToDonate"
+          className={
+            proprety[0] === ".myactive" ? styles.myactive : styles.mynone
+          }
+        />
+        <img
+          src="https://static1.bigstockphoto.com/3/6/9/large1500/96302606.jpg"
+          alt="ProudToDonate2"
+          className={
+            proprety[1] === ".myactive" ? styles.myactive : styles.mynone
+          }
+        />
+      </div>
     </React.Fragment>
   );
 };
