46ad865d50be02ab22736110e62ca2ec60f1449d
Vivek-Kamboj <vivekkamboj4555@gmail.com>
2021-01-02 12:15:04 +0530

    Basic styling and editing

--- Frontend/crowd-funding-frontend/src/Routes/adminDashboard.jsx
+++ Frontend/crowd-funding-frontend/src/Routes/adminDashboard.jsx
@@ -10,45 +10,43 @@ const AdminDashboard = (p) => {
   return (
     <React.Fragment>
       <NavBar />
-      <div>AdminDashboard</div>
-      <button
-        className="btn btn-warning m-2"
-        onClick={() => {
-          p.history.push("/admin/new");
-        }}
-      >
-        New Admin +
-      </button>
-      <button
-        className="btn btn-warning"
-        onClick={() => {
-          p.history.push("/admin/campaign/new");
-        }}
-      >
-        New Campaign
-      </button>
-      <button
-        className="btn btn-primary m-2"
-        onClick={() => {
-          p.history.push("/all-campaigns");
-        }}
-      >
-        All Campaign
-      </button>
-      <button
-        className="btn btn-danger"
-        onClick={() => {
-          logout().then(() => {
-            window.location = "/";
-          });
-        }}
-      >
-        Logout
-      </button>
-      <h6>Admin say for refrence...</h6>
-      <p>
-        email:x@iiitm.ac.in <br /> password:1234
-      </p>
+      <div className="col-12 col-md-10">
+        <div>AdminDashboard</div>
+        <button
+          className="btn btn-warning m-2"
+          onClick={() => {
+            p.history.push("/admin/new");
+          }}
+        >
+          New Admin +
+        </button>
+        <button
+          className="btn btn-warning"
+          onClick={() => {
+            p.history.push("/admin/campaign/new");
+          }}
+        >
+          New Campaign
+        </button>
+        <button
+          className="btn btn-primary m-2"
+          onClick={() => {
+            p.history.push("/all-campaigns");
+          }}
+        >
+          All Campaign
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            logout().then(() => {
+              window.location = "/";
+            });
+          }}
+        >
+          Logout
+        </button>
+      </div>
     </React.Fragment>
   );
 };
