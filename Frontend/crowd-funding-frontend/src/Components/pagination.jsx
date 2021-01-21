08aae5bb340b232bb5689a4fbff5cd84ee5b8846
Vivek-Kamboj <vivekkamboj4555@gmail.com>
2021-01-21 17:39:02 +0530

    Resolved paginate overflow issue

--- Frontend/crowd-funding-frontend/src/Components/pagination.jsx
+++ Frontend/crowd-funding-frontend/src/Components/pagination.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import _ from "lodash";
 import styles from "./styles/paginate.module.css";
 
 const Pagination = (props) => {
+  const ref = useRef();
+
+  const handleScroll = (direction) => {
+    if (direction === "left") {
+      if (ref) {
+        ref.current.scrollLeft -= 40;
+      }
+    } else {
+      if (ref) {
+        ref.current.scrollLeft += 40;
+      }
+    }
+  };
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
 
   const pagesCount = Math.ceil(itemsCount / pageSize);
@@ -13,10 +26,11 @@ const Pagination = (props) => {
   return (
     <React.Fragment>
       <nav className={styles.nav}>
-        <ul className={`pagination ${styles.ul}`}>
-          {/* <li className="page-item">
-            <button className={`page-link ${styles.inactive}`}>Previous</button>
-          </li> */}
+        <ul
+          className={`pagination ${styles.ul}`}
+          ref={ref}
+          style={{ width: "200px", overflow: "scroll" }}
+        >
           {pages.map((page) => (
             <li
               className={
@@ -37,11 +51,24 @@ const Pagination = (props) => {
               </button>
             </li>
           ))}
-          {/* ...
-          <li className="page-item">
-            <button className={`page-link ${styles.inactive}`}> Next</button>
-          </li> */}
         </ul>
+        {pagesCount > 6 && (
+          <div style={{ margin: "auto", textAlign: "center" }}>
+            <button
+              className={`btn btn-success ${styles.active}`}
+              onClick={() => handleScroll("left")}
+            >
+              <i className="fa fa-chevron-left" aria-hidden="true"></i>
+            </button>
+
+            <button
+              className={`btn btn-success ${styles.active}`}
+              onClick={() => handleScroll("right")}
+            >
+              <i className="fa fa-chevron-right" aria-hidden="true"></i>
+            </button>
+          </div>
+        )}
       </nav>
     </React.Fragment>
   );
