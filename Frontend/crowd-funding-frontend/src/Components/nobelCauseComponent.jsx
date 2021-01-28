4b1eb8c7e2b5a65ef324161406d82ef8af497ec2
Swatik <swatikpl44@gmail.com>
2021-01-28 18:57:31 +0530

    Add Images to Assets

--- Frontend/crowd-funding-frontend/src/Components/nobelCauseComponent.jsx
+++ Frontend/crowd-funding-frontend/src/Components/nobelCauseComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import nobleCause from "./assets/nobleCause.jpg";
 import styles from "./styles/nobelCauseComponent.module.css";
 
 const NobelCauseComponent = () => {
@@ -12,11 +13,7 @@ const NobelCauseComponent = () => {
           architecto voluptates a dolore odit quibusdam recusandae tempore iusto
           voluptatem.
         </p>
-        <img
-          className={styles.image}
-          src="https://images.unsplash.com/photo-1573919639277-5d481e497833?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"
-          alt="noble cause"
-        />
+        <img className={styles.image} src={nobleCause} alt="noble cause" />
       </div>
     </React.Fragment>
   );
