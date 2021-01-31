7865587ccddf5c460f7edcffc5fb4bc283a254fa
Swatik <swatikpl44@gmail.com>
2021-01-31 13:04:44 +0530

    Fix Footer Styling

--- Frontend/crowd-funding-frontend/src/Components/footer.jsx
+++ Frontend/crowd-funding-frontend/src/Components/footer.jsx
@@ -1,73 +1,102 @@
 import React from "react";
 // import { Link } from "react-router-dom";
+import logo from "./assets/RotaractIcon.png";
 import styles from "./styles/footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={`mb-0 p-4 ${styles.footer}`}>
-      <div class="row col-12">
-        <div className={`col-md-8 ${styles.about}`}>
-          <h2 className={`${styles.header1}`}>ROTORACT CLUB</h2>
-          <p class="text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam.
-            <br />
-            <br />
-            For any queries, reach out to us at email :{" "}
-            <b>rotoract@iiitm.ac.in</b>
+      <div class="row">
+        <div className={`col-12 col-sm-6 col-md-4 ${styles.left}`}>
+          <img className={`${styles.logo}`} src={logo} alt="ROTORACT LOGO" />
+          <br />
+          <p className={`${styles.caption}`}>
+            Rotaract Club <br /> IIITM Gwalior
           </p>
         </div>
-
-        <div className={`col-md-4 ${styles.links}`}>
-          <h4 className={`${styles.header2}`}>QUICK LINKS</h4>
-          <p className={`${styles.linkList}`}>
+        <div className={`col-12 col-sm-6 col-md-4 ${styles.middle}`}>
+          <p>Follow us on</p>
+          <a
+            className={`${styles.facebook}`}
+            href="https://www.facebook.com/rotaractiiitm/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-facebook"></i>
+          </a>
+          <a
+            className={`${styles.linkedin}`}
+            href="https://in.linkedin.com/company/rotaract-club-abv-iiitm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-linkedin"></i>
+          </a>
+          <br />
+          <div className={`${styles.links}`}>
             <a className={`${styles.link}`} href="/about-us">
-              About Us
+              About
             </a>
-          </p>
-          <p className={`${styles.linkList}`}>
             <a className={`${styles.link}`} href="/contact-us">
               Contact Us
             </a>
-          </p>
-          <p className={`${styles.linkList}`}>
-            <a className={`${styles.link}`} href="/all-campaigns">
-              Browse Campaigns
+            <a
+              className={`${styles.link}`}
+              href="https://www.iiitm.ac.in/index.php/en/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Institute
             </a>
+          </div>
+        </div>
+        <div className={`col-12 col-sm-12 col-md-4 ${styles.right}`}>
+          <p className={`${styles.names}`}>
+            <span style={{ fontWeight: "bolder" }}>Developed By : </span>
+            <br />
+            <a
+              className={`${styles.name}`}
+              href="https://in.linkedin.com/in/swatik-paul-1218b3136"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Swatik Paul
+            </a>
+            <br />
+            <a
+              className={`${styles.name}`}
+              href="https://www.linkedin.com/in/vivekkamboj1/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Vivek Kamboj
+            </a>
+            <br />
+            <br />
+            <span style={{ fontWeight: "bolder" }}>Designed By : </span>
+            <br />
+            <a
+              className={`${styles.name}`}
+              href="https://in.linkedin.com/in/manish-mavi-05"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Manish Mavi
+            </a>
+          </p>
+          <p>
+            For any queries, reach out to us at email :
+            <br />
+            <b>rotoract@iiitm.ac.in</b>
           </p>
         </div>
       </div>
 
       <div className={`${styles.footerBottom}`}>
         <hr className={`${styles.line}`} />
-        <div class="row col-12">
-          <div className={`col-md-8 ${styles.copyrightText}`}>
-            <p>
-              Copyright &copy; 2021 All Rights Reserved by ROTORACT CLUB @
-              IIITM.
-            </p>
-          </div>
-
-          <div className={`col-md-4 ${styles.icons}`}>
-            <a
-              className={`${styles.facebook}`}
-              href="https://www.facebook.com/rotaractiiitm/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i class="fa fa-facebook"></i>
-            </a>
-            <a
-              className={`${styles.linkedin}`}
-              href="https://in.linkedin.com/company/rotaract-club-abv-iiitm"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i class="fa fa-linkedin"></i>
-            </a>
-          </div>
-        </div>
+        <p style={{ textAlign: "center" }}>
+          Copyright &copy; 2021 ROTORACT CLUB IIITM. All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
